7d21d21f82b591b2aa09ce0193a57ecfb11f879c
OlehLiapota <oleh.liapota@nure.ua>
2024-06-19 19:22:18 +0300

    Unary minus and leading zero handling

--- src/main/java/ua/nure/calculator/service/CalculatorService.java
+++ src/main/java/ua/nure/calculator/service/CalculatorService.java
@@ -28,7 +28,6 @@ public class CalculatorService {
                 variables.put(varName, evaluateExpression(varValue));
             } else if (line.matches("^\\s*\\?\\s*[a-zA-Z]\\s*$")) {
                 String varName = line.trim().substring(1).trim();
-
                 if (variables.containsKey(varName)) {
                     return evaluateExpression(variables.get(varName));
                 }
@@ -41,81 +40,90 @@ public class CalculatorService {
     }
 
     private String evaluateExpression(String expression) {
-        Queue<String> infixExpression = infixToPostfix(expression);
-        Stack<String> stack = new Stack<>();
+        try {
+            Queue<String> infixExpression = infixToPostfix(expression);
 
-        for (String i : infixExpression) {
-            if (precedence.containsKey(i)) {
-                String b = stack.pop();
-                String a = stack.pop();
+            Stack<String> stack = new Stack<>();
 
-                if (!b.matches("-?\\d+(\\.\\d+)?")
-                    || !a.matches("-?\\d+(\\.\\d+)?")) {
-                    stack.push("(" + a + i + b + ")");
-                } else {
-                    switch (i) {
-                        case "+":
-                            stack.push(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));
-                            break;
-                        case "-", "–":
-                            stack.push(String.valueOf(Double.parseDouble(a) - Double.parseDouble(b)));
-                            break;
-                        case "*":
-                            stack.push(String.valueOf(Double.parseDouble(a) * Double.parseDouble(b)));
-                            break;
-                        case "/":
-                            stack.push(String.valueOf(Double.parseDouble(a) / Double.parseDouble(b)));
-                            break;
-                        case "^":
-                            stack.push(String.valueOf(Math.pow(Double.parseDouble(a), Double.parseDouble(b))));
-                            break;
+            for (String i : infixExpression) {
+                if (precedence.containsKey(i)) {
+                    String b = stack.pop();
+                    String a = stack.pop();
+
+                    if (!b.matches("-?\\d+(\\.\\d+)?") || !a.matches("-?\\d+(\\.\\d+)?")) {
+                        stack.push("(" + a + i + b + ")");
+                    } else {
+                        switch (i) {
+                            case "+":
+                                stack.push(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));
+                                break;
+                            case "-", "–":
+                                stack.push(String.valueOf(Double.parseDouble(a) - Double.parseDouble(b)));
+                                break;
+                            case "*":
+                                stack.push(String.valueOf(Double.parseDouble(a) * Double.parseDouble(b)));
+                                break;
+                            case "/":
+                                stack.push(String.valueOf(Double.parseDouble(a) / Double.parseDouble(b)));
+                                break;
+                            case "^":
+                                stack.push(String.valueOf(Math.pow(Double.parseDouble(a), Double.parseDouble(b))));
+                                break;
+                        }
                     }
+                } else {
+                    stack.push(i);
                 }
-            } else {
-                stack.push(i);
             }
-        }
 
-        return stack.pop();
+            return stack.pop();
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
     }
 
-    private Queue<String> infixToPostfix(String expression) {
+    private Queue<String> infixToPostfix(String expression) throws IllegalArgumentException {
         String[] tokens = expression.split("(?<=[-+*/^()\\u2013])|(?=[-+*/^()\\u2013])|(?<=\\b)(?=\\b)\n");
-
         Queue<String> output = new ArrayDeque<>();
         Stack<String> operators = new Stack<>();
+        boolean lastTokenWasOperator = true;
 
         for (String t : tokens) {
             String fixedToken = t.trim();
+
+            if (fixedToken.matches(".*\\b0\\d+\\b.*")) {
+                throw new IllegalArgumentException("Leading zeros are not allowed in numbers");
+            }
 
             if (!fixedToken.isEmpty() && fixedToken.charAt(fixedToken.length() - 1) == ';') {
                 fixedToken = fixedToken.substring(0, fixedToken.length() - 1);
             }
 
-            if (fixedToken.matches("^-?\\d+$")
-                    || fixedToken.matches("^-?\\d*\\.\\d+$")) {
+            if (fixedToken.matches("^-?\\d+$") || fixedToken.matches("^-?\\d*\\.\\d+$")) {
                 output.offer(fixedToken);
+                lastTokenWasOperator = false;
             } else if (fixedToken.matches("^[a-zA-Z]$")) {
-                output.offer(variables.containsKey(fixedToken)
-                        ? evaluateExpression(variables.get(fixedToken))
-                        : fixedToken);
+                output.offer(variables.containsKey(fixedToken) ? evaluateExpression(variables.get(fixedToken)) : fixedToken);
+                lastTokenWasOperator = false;
             } else if (fixedToken.equals("(")) {
                 operators.push(fixedToken);
+                lastTokenWasOperator = true;
             } else if (fixedToken.equals(")")) {
-                while (!operators.isEmpty()
-                        && !operators.peek().equals("(")) {
+                while (!operators.isEmpty() && !operators.peek().equals("(")) {
                     output.offer(operators.pop());
                 }
-
                 operators.pop();
+                lastTokenWasOperator = false;
             } else if (precedence.containsKey(fixedToken)) {
-                while (!operators.isEmpty()
-                        && precedence.getOrDefault(operators.peek(), 0)
-                        >= precedence.get(fixedToken)) {
+                if (fixedToken.equals("-") && lastTokenWasOperator) {
+                    output.offer("-1");
+                    fixedToken = "*";
+                }
+                while (!operators.isEmpty() && precedence.getOrDefault(operators.peek(), 0) >= precedence.get(fixedToken)) {
                     output.offer(operators.pop());
                 }
-
                 operators.push(fixedToken);
+                lastTokenWasOperator = true;
             }
         }
 
